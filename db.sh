8df79fa8de13d53b1959f4363f6f30bcc05d7a68
Christopher Juchem <christopher.juchem@datarobot.com>
2022-07-16 12:31:58 -0400

    local db

---
+++ db.sh
@@ -0,0 +1,2 @@
+PGPASSWORD=blacklotus psql -h 0.0.0.0 -p 5432 -U local_db_user -d mtgvrd -w
+
---
+++ first_time_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This file must be sourced or run `. venv/bin/activate` afterwards
+
+python3.8 -m virtualenv venv
+. venv/bin/activate
+
+echo "run `. venv/bin/activate` to begin working"
